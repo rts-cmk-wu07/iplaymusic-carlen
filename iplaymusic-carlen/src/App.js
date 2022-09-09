79fe25de5268e09d4bdead7e46ad7526875c2fbd
Mark Brodersen <U2405963@rts-365.dk>
2022-09-09 10:57:06 +0200

    vars opsat

--- iplaymusic-carlen/src/App.js
+++ iplaymusic-carlen/src/App.js
@@ -1,5 +1,30 @@
+import { vars } from "./vars";
+/** @jsxImportSource @emotion/react */
+import { css } from "@emotion/react";
+import { useState } from "react";
+
 function App() {
-  return <div className="App"></div>
+  const { light, dark } = vars;
+  const [theme, setTheme] = useState(light);
+  const handleThemeChange = () => {
+    if (theme === light) {
+      setTheme(dark);
+    } else {
+      setTheme(light);
+    }
+  };
+  const colors = theme;
+  const styles = {
+    bg: css`
+      background-color: ${colors.primary};
+    `,
+  };
+  return (
+    <div className="App">
+      <div css={styles.bg}>hejsa</div>
+      <button onClick={handleThemeChange}>hejsa</button>
+    </div>
+  );
 }
 
-export default App
+export default App;
---
+++ iplaymusic-carlen/src/vars.js
@@ -0,0 +1,16 @@
+export const vars = {
+  dark: {
+    primary: "#341931",
+    secondary: "white",
+    tertiary: "gray",
+    text: "black",
+    bg: "white",
+  },
+  light: {
+    primary: "#ff1168",
+    secondary: "black",
+    tertiary: "gray",
+    text: "white",
+    bg: "black",
+  },
+};
