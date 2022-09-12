e66b35aef78694eedc8ad0381670fe546b373280
cookieman2002 <mcl2002@outlook.dk>
2022-09-12 10:06:31 +0200

    gradient button

--- iplaymusic-carlen/src/comp/sub-comp/Playbutton.js
+++ iplaymusic-carlen/src/comp/sub-comp/Playbutton.js
@@ -12,11 +12,14 @@ const Playbutton = () => {
 
   return (
     <button
-      className="rounded-full border border-solid border-primary-100 p-3"
+      className="rounded-full border border-solid border-primary-100 p-3 from-gradient-100 to-gradient-200 bg-gradient-to-r"
       onClick={buttonPlayer}
     >
-      
-      <FeatherIcon icon={isPlaying ? "pause" : "play"} />
+      <FeatherIcon
+        icon={isPlaying ? "pause" : "play"}
+        fill="white"
+        stroke="white"
+      />
     </button>
   );
 };
