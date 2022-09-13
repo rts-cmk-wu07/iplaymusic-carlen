325abb363f66fb4afc5b3c7d235c6d5bc58e35f1
CarlAbel <caweb.cph@gmail.com>
2022-09-13 14:09:52 +0200

    added heading

--- iplaymusic-carlen/src/pages/AllAlbums.js
+++ iplaymusic-carlen/src/pages/AllAlbums.js
@@ -1,9 +1,11 @@
 import NewReleases from "../comp/NewReleases"
 import SliderAlbums from "../comp/SliderAlbums"
+import Heading from "../comp/sub-comp/Heading"
 
 export default function AllAlbums() {
   return (
     <div>
+      <Heading className="text-3xl font-bold" />
       <SliderAlbums />
       <NewReleases />
     </div>
