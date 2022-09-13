077366080cd1d75544e5c16a7ae05f1d02473944
Mark Brodersen <U2405963@rts-365.dk>
2022-09-13 09:05:16 +0200

    IonIcons Added

--- iplaymusic-carlen/src/comp/BackBtn.js
+++ iplaymusic-carlen/src/comp/BackBtn.js
@@ -1,9 +1,23 @@
-import FeatherIcon from "feather-icons-react";
+/** @jsxImportSource @emotion/react */
+import { css } from "@emotion/react";
+import IonIcon from "@reacticons/ionicons";
 const BackBtn = () => {
+  let currentcolor = "#000000";
+  const styles = {
+    icon: css`
+      color: black;
+      fill: black;
+      stroke: black;
+    `,
+  };
   function backHandler() {}
   return (
-    <button>
-      <FeatherIcon icon="chevron-left" />
+    <button css={styles.icon}>
+      <IonIcon
+        name="chevron-back-circle-sharp "
+        size="large"
+        css={styles.icon}
+      />
     </button>
   );
 };
