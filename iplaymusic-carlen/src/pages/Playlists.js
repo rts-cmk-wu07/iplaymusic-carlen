49a4f8977b16cbe69efd1520937ce5f6c0cd5d17
CarlAbel <caweb.cph@gmail.com>
2022-09-16 13:09:16 +0200

    added playlistsSongs

--- iplaymusic-carlen/src/pages/Playlists.js
+++ iplaymusic-carlen/src/pages/Playlists.js
@@ -1,9 +1,11 @@
+import PlaylistsSongs from "../comp/PlaylistsSongs"
 import Heading from "../comp/sub-comp/Heading"
 
 export default function Playlists() {
   return (
     <div>
       <Heading className=" mb-3" text="Playlists"></Heading>
+      <PlaylistsSongs />
     </div>
   )
 }
