138711bf834caa3615a7eaa019025e51f5c5a8b4
CarlAbel <caweb.cph@gmail.com>
2022-09-16 12:49:56 +0200

    insert Heading

---
+++ iplaymusic-carlen/src/pages/Playlists.js
@@ -0,0 +1,9 @@
+import Heading from "../comp/sub-comp/Heading"
+
+export default function Playlists() {
+  return (
+    <div>
+      <Heading className=" mb-3" text="Playlists"></Heading>
+    </div>
+  )
+}
