8a069aa9ed318af2c2aa72310e219e2b368034d1
Mark Brodersen <U2405963@rts-365.dk>
2022-09-16 08:56:56 +0200

    Home with links

--- iplaymusic-carlen/src/App.js
+++ iplaymusic-carlen/src/App.js
@@ -1,30 +1,32 @@
-import { vars } from "./vars"
-import { useState } from "react"
-import ColorContext from "./context/colorContext"
-import { BrowserRouter, Route, Routes } from "react-router-dom"
-import Layout from "./templates/Layout"
-import Playbutton from "./comp/sub-comp/Playbutton"
-import HandleColorChange from "./context/handleColorChange"
-import AllAlbums from "./pages/AllAlbums"
-import EventFeed from "./pages/EventFeed"
+import { vars } from "./vars";
+import { useState } from "react";
+import ColorContext from "./context/colorContext";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Layout from "./templates/Layout";
+import Playbutton from "./comp/sub-comp/Playbutton";
+import HandleColorChange from "./context/handleColorChange";
+import AllAlbums from "./pages/AllAlbums";
+import EventFeed from "./pages/EventFeed";
+import Home from "./pages/Home";
 
 function App() {
-  const { light, dark } = vars
-  const [theme, setTheme] = useState(light)
+  const { light, dark } = vars;
+  const [theme, setTheme] = useState(light);
   const handleThemeChange = () => {
     if (theme === light) {
-      setTheme(dark)
+      setTheme(dark);
     } else {
-      setTheme(light)
+      setTheme(light);
     }
-  }
-  const colors = theme
+  };
+  const colors = theme;
   return (
     <HandleColorChange.Provider value={handleThemeChange}>
       <ColorContext.Provider value={colors}>
         <BrowserRouter>
           <Routes>
             <Route path="/" element={<Layout />}>
+              <Route index element={<Home />} />
               <Route path="/eventfeed" element={<EventFeed />} />
               <Route path="/allalbums" element={<AllAlbums />}></Route>
             </Route>
@@ -32,7 +34,7 @@ function App() {
         </BrowserRouter>
       </ColorContext.Provider>
     </HandleColorChange.Provider>
-  )
+  );
 }
 
-export default App
+export default App;
---
+++ iplaymusic-carlen/src/pages/Home.js
@@ -0,0 +1,12 @@
+import Links from "../comp/Links";
+
+const Home = () => {
+  return (
+    <div>
+      <Links link="/eventfeed" linkText="Event feed" />
+      <Links link="/allalbums" linkText="All Albums" />
+    </div>
+  );
+};
+
+export default Home;
