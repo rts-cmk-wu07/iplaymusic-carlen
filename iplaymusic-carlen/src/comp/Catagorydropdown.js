f3e87c35682f47fc000eb62a010cf66a27e7bcf8
cookieman2002 <mcl2002@outlook.dk>
2022-09-16 09:53:35 +0200

    catagory almost done need api

--- iplaymusic-carlen/src/comp/Catagorydropdown.js
+++ iplaymusic-carlen/src/comp/Catagorydropdown.js
@@ -1,8 +1,13 @@
-import { useState } from "react";
-import FeatherIcon from "feather-icons-react";
-import { Disclosure } from "@headlessui/react";
+/** @jsxImportSource @emotion/react */
+import { css } from "@emotion/react";
+
+import { Disclosure, Transition } from "@headlessui/react";
 import CatagorySongBrand from "./sub-comp/Catagorysongbrand";
-const CatagoryDropdown = ({ title, songname, catcolor }) => {
+import IonIcon from "@reacticons/ionicons";
+import { useContext } from "react";
+import ColorContext from "../context/colorContext";
+
+const CatagoryDropdown = ({ title, songname, catcolor, songtitle }) => {
   // let topics = songname.map((topic) => topic.type);
   // //console.log(topics)
   // let sortedTopics = topics.filter((element, index) => {
@@ -21,14 +26,40 @@ const CatagoryDropdown = ({ title, songname, catcolor }) => {
   //     </Disclosure.Panel>
   //   </Disclosure>
   // ))
+  const colors = useContext(ColorContext)
+const styles = {
+  title: css`
+  color: ${colors.text};
+  `,
+
+}
+
   return (
     <div className="flex flex-col">
       {title ? (
         <Disclosure>
-          <Disclosure.Button className={`${catcolor} text-2xl text-white justify-start`}>{title}</Disclosure.Button>
-          <Disclosure.Panel>
-            <CatagorySongBrand songname={songname} />
+          {({open}) => (
+            <>
+            <Disclosure.Button className={`${catcolor} flex justify-between items-center text-2xl text-white pb-5 pt-5 `}>
+              {title}
+              <IonIcon name="ellipsis-horizontal"/>
+              </Disclosure.Button>
+              <Transition
+        enter="transition duration-100 ease-out"
+        enterFrom="transform scale-95 opacity-0"
+        enterTo="transform scale-100 opacity-100"
+        leave="transition duration-75 ease-out"
+        leaveFrom="transform scale-100 opacity-100"
+        leaveTo="transform scale-95 opacity-0"
+      >
+
+          <Disclosure.Panel css={styles.title}>
+            
+            <CatagorySongBrand songname={songname} songtitle={songtitle} />
           </Disclosure.Panel>
+      </Transition>
+            </>
+            )}
         </Disclosure>
       ) : (
         <span>nothing avalible</span>
--- iplaymusic-carlen/src/pages/Catagory.js
+++ iplaymusic-carlen/src/pages/Catagory.js
@@ -47,14 +47,14 @@ const Catagory = () => {
     
     return ( <div className="flex flex-col">
         <h1 className="font-extrabold text-transparent text-4xl bg-clip-text bg-gradient-to-r from-primary-100 to-primary-200 pb-5">Catagory</h1>
-        <CatagoryDropdown title="Alternative" catcolor="bg-primary-100" songname="POP"/>
-        <CatagoryDropdown title="Classic" songname="Dance" catcolor="bg-catColor-800"/>
-        <CatagoryDropdown title="Classic" songname="Dance" catcolor="bg-gradient-200"/>
-        <CatagoryDropdown title="Classic" songname="Dance" catcolor="bg-catColor-700"/>
-        <CatagoryDropdown title="Classic" songname="Dance" catcolor="bg-catColor-500"/>
-        <CatagoryDropdown title="Classic" songname="Dance" catcolor="bg-catColor-400"/>
-        <CatagoryDropdown title="Classic" songname="Dance" catcolor="bg-catColor-300"/>
-        <CatagoryDropdown title="Classic" songname="Dance" catcolor="bg-catColor-200"/>
+        <CatagoryDropdown title="Alternative" catcolor="bg-primary-100" songname="POP" songtitle="Set the rain on fire"/>
+        <CatagoryDropdown title="Classic" songname="Dance" catcolor="bg-catColor-800" songtitle="Minecraft"/>
+        <CatagoryDropdown title="Classic" songname="Dance" catcolor="bg-gradient-200" songtitle="Minecraft"/>
+        <CatagoryDropdown title="Classic" songname="Dance" catcolor="bg-catColor-700" songtitle="Minecraft"/>
+        <CatagoryDropdown title="Classic" songname="Dance" catcolor="bg-catColor-500" songtitle="Minecraft"/>
+        <CatagoryDropdown title="Classic" songname="Dance" catcolor="bg-catColor-400" songtitle="Minecraft"/>
+        <CatagoryDropdown title="Classic" songname="Dance" catcolor="bg-catColor-300" songtitle="Minecraft"/>
+        <CatagoryDropdown title="Classic" songname="Dance" catcolor="bg-catColor-200" songtitle="Minecraft"/>
     </div> );
 }
  
