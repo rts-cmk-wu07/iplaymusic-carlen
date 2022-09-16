df7eccd750a92d4c2379d75018395030a65df7a5
CarlAbel <caweb.cph@gmail.com>
2022-09-16 10:04:12 +0200

    home update Link

--- iplaymusic-carlen/src/pages/Home.js
+++ iplaymusic-carlen/src/pages/Home.js
@@ -1,4 +1,4 @@
-import Links from "../comp/Links";
+import Links from "../comp/Links"
 
 const Home = () => {
   return (
@@ -6,8 +6,9 @@ const Home = () => {
       <Links link="/eventfeed" linkText="Event feed" />
       <Links link="/catagory" linkText="Catagory" />
       <Links link="/allalbums" linkText="All Albums" />
+      <Links link="/allartists" linkText="All Artists" />
     </div>
-  );
-};
+  )
+}
 
-export default Home;
+export default Home
