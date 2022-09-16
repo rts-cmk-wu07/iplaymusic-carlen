bb8a09b1af11dd9395f53f45b826e6d41108e1a1
CarlAbel <caweb.cph@gmail.com>
2022-09-16 12:49:15 +0200

    first commit

--- iplaymusic-carlen/src/App.js
+++ iplaymusic-carlen/src/App.js
@@ -1,27 +1,28 @@
-import { vars } from "./vars";
-import { useState } from "react";
-import ColorContext from "./context/colorContext";
-import CatagoryDropdown from "./comp/Catagorydropdown";
-import { BrowserRouter, Route, Routes } from "react-router-dom";
-import Layout from "./templates/Layout";
-import Playbutton from "./comp/sub-comp/Playbutton";
-import Catagory from "./pages/Catagory";
-import HandleColorChange from "./context/handleColorChange";
-import AllAlbums from "./pages/AllAlbums";
-import EventFeed from "./pages/EventFeed";
-import Home from "./pages/Home";
+import { vars } from "./vars"
+import { useState } from "react"
+import ColorContext from "./context/colorContext"
+import CatagoryDropdown from "./comp/Catagorydropdown"
+import { BrowserRouter, Route, Routes } from "react-router-dom"
+import Layout from "./templates/Layout"
+import Playbutton from "./comp/sub-comp/Playbutton"
+import Catagory from "./pages/Catagory"
+import HandleColorChange from "./context/handleColorChange"
+import AllAlbums from "./pages/AllAlbums"
+import EventFeed from "./pages/EventFeed"
+import Home from "./pages/Home"
+import Playlists from "./pages/Playlists"
 
 function App() {
-  const { light, dark } = vars;
-  const [theme, setTheme] = useState(light);
+  const { light, dark } = vars
+  const [theme, setTheme] = useState(light)
   const handleThemeChange = () => {
     if (theme === light) {
-      setTheme(dark);
+      setTheme(dark)
     } else {
-      setTheme(light);
+      setTheme(light)
     }
-  };
-  const colors = theme;
+  }
+  const colors = theme
   return (
     <HandleColorChange.Provider value={handleThemeChange}>
       <ColorContext.Provider value={colors}>
@@ -30,14 +31,17 @@ function App() {
             <Route path="/" element={<Layout />}>
               <Route index element={<Home />} />
               <Route path="/eventfeed" element={<EventFeed />} />
-              <Route path="/catagory" element={<Catagory />}> </Route>
-              <Route path="/allalbums" element={<AllAlbums />}></Route>
+              <Route path="/catagory" element={<Catagory />}>
+                {" "}
+              </Route>
+              <Route path="/allalbums" element={<AllAlbums />} />
+              <Route path="/playlists" element={<Playlists />} />
             </Route>
           </Routes>
         </BrowserRouter>
       </ColorContext.Provider>
     </HandleColorChange.Provider>
-  );
+  )
 }
 
-export default App;
+export default App
