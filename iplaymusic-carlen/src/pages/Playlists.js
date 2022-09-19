324e88dc81eb8f25dff2b8786324f9a37e45d706
CarlAbel <caweb.cph@gmail.com>
2022-09-19 13:59:57 +0200

    add slider

--- iplaymusic-carlen/src/pages/Playlists.js
+++ iplaymusic-carlen/src/pages/Playlists.js
@@ -1,10 +1,12 @@
 import PlaylistsSongs from "../comp/PlaylistsSongs"
+import SliderAlbums from "../comp/SliderAlbums"
 import Heading from "../comp/sub-comp/Heading"
 
 export default function Playlists() {
   return (
     <div>
-      <Heading className=" mb-3" text="Playlists"></Heading>
+      <Heading className="mb-3" text="Playlists"></Heading>
+      <SliderAlbums />
       <PlaylistsSongs />
     </div>
   )
