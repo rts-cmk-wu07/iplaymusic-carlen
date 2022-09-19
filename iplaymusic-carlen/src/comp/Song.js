4184a5a2e54c93634c5757abebc5a093008ac122
CarlAbel <caweb.cph@gmail.com>
2022-09-19 10:53:47 +0200

    tailwind update small changes

--- iplaymusic-carlen/src/comp/Song.js
+++ iplaymusic-carlen/src/comp/Song.js
@@ -11,17 +11,21 @@ const Song = ({ songtitle, songtime, artist }) => {
     `,
   }
   return (
-    <div className="flex pl-1 mb-4">
+    <div className="flex pl-4 mb-4">
       <PlayButton
         className="w-20 m-3 
               "
       />
       <div className="flex justify-between w-72 items-center">
-        <div className="flex-col w-40 ml-2">
-          <span css={styles.theme}>{songtitle}</span>
-          <span css={styles.theme}>{artist}</span>
+        <div className="flex flex-col w-full ml-3">
+          <span className="font-semibold pb-1" css={styles.theme}>
+            {songtitle}
+          </span>
+          <span className="pt-1" css={styles.theme}>
+            {artist}
+          </span>
         </div>
-        <div className="flex pr-6 font-light text-sm">
+        <div className="flex pr-2 font-light text-sm">
           <span css={styles.theme}>{songtime}</span>
         </div>
       </div>
