6252a6da39eebc738d9269dd719be240829e52ed
Carl Abel <caweb.cph@gmail.com>
2022-09-20 14:15:19 +0200

    Link All artist

--- iplaymusic-carlen/src/pages/Home.js
+++ iplaymusic-carlen/src/pages/Home.js
@@ -6,7 +6,7 @@ const Home = () => {
       <Links color="blue" link="/eventfeed" linkText="Event feed" />
       <Links color="green" link="/catagory" linkText="Catagory" />
       <Links color="red" link="/allalbums" linkText="All Albums" />
-      <Links link="/allartists" linkText="All Artists" />
+      <Links color="green" link="/allartists" linkText="All Artists" />
       <Links color="red" link="/albumdetails" linkText="Albums Details" />
       <Links color="blue" link="/playlists" linkText="Playlists" />
     </div>
