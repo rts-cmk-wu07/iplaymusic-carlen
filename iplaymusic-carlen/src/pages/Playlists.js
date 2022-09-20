b2f23860be3a405eab3adef3d32ae86cc568e8c7
Carl Abel <caweb.cph@gmail.com>
2022-09-20 11:39:17 +0200

    clip path update

--- iplaymusic-carlen/src/pages/Playlists.js
+++ iplaymusic-carlen/src/pages/Playlists.js
@@ -1,13 +1,25 @@
-import PlaylistsSongs from "../comp/PlaylistsSongs"
-import SliderAlbums from "../comp/SliderAlbums"
-import Heading from "../comp/sub-comp/Heading"
+/** @jsxImportSource @emotion/react */
+import { css } from "@emotion/react";
+import PlaylistsSongs from "../comp/PlaylistsSongs";
+import SliderAlbums from "../comp/SliderAlbums";
+import Heading from "../comp/sub-comp/Heading";
 
 export default function Playlists() {
+  const styles = {
+    bgImage: css`
+      padding: 12px;
+      background: url(http://placekitten.com/351/300);
+      height: 300px;
+    `,
+    theme: css`
+      color: ${colors.text};
+    `,
+  };
   return (
     <div>
       <Heading className="mb-3" text="Playlists"></Heading>
-      <SliderAlbums />
+      <SliderAlbums css={styles.bgImage} />
       <PlaylistsSongs />
     </div>
-  )
+  );
 }
