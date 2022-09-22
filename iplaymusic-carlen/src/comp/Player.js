5a46e4bd53fc23d070fad35e10a0f9cb2dc8c2bb
cookieman2002 <mcl2002@outlook.dk>
2022-09-22 13:51:34 +0200

    player works selection missing

--- iplaymusic-carlen/src/comp/Player.js
+++ iplaymusic-carlen/src/comp/Player.js
@@ -4,22 +4,24 @@ import IonIcon from "@reacticons/ionicons";
 import { useState } from "react";
 import ReactAudioPlayer from "react-audio-player";
 import AudioPlayer from "react-h5-audio-player";
-import "react-h5-audio-player/lib/styles.css";
+import "./Player.css";
 import { useContext } from "react";
-import ColorContext from "../context/colorContext"
-
+import ColorContext from "../context/colorContext";
 
 const Player = () => {
-  const colors = useContext(ColorContext)
+  const colors = useContext(ColorContext);
   const [isPlaying, setIsPlaying] = useState(false);
   const musicTracks = [
     {
       name: "name",
       src: "https://github.com/cookieman2002/iplay-music-mp3/blob/main/Cantina%20Band.mp3?raw=true",
+      creator: "adele"
     },
+
     {
       name: "Mystery",
       src: "http://github.com/cookieman2002/iplay-music-mp3/blob/main/bensound-memories.mp3?raw=true",
+      creator: "master oogway"
     },
   ];
 
@@ -46,15 +48,9 @@ const Player = () => {
         border-radius: 0px;
         width: 100%;
       }
-      
     `,
-    overlay: css`
-    background-color: ${colors.bg};
-
-    && span{
-      color: ${colors.text}
-    }
-    `,
+    
+    
   };
   function playerHandler() {
     setIsPlaying(!isPlaying);
@@ -65,30 +61,34 @@ const Player = () => {
       css={styles.audioplayer}
       className="flex justify-center flex-col items-center"
     >
-      {isPlaying && (
-        <div css={styles.overlay} className="absolute z-10 bg-gradient-to-r top-0  w-screen h-screen">
-          <IonIcon name="close" className="text-3xl"  onClick={() => playerHandler(!isPlaying)} />
-          <div>
-
-          </div>
-        </div>
-      )}
-      <div>
-
       <span onClick={playerHandler}>Extend</span>
-      <AudioPlayer
-        style={{ background: "linear-gradient(#ee0979, #341931)" }}
-        autoPlay
-        src={musicTracks[isTracking].src}
-        onPlay={musicTracks[isTracking].src}
-        header={`now playing ${musicTracks[isTracking].name}`}
-        onClickPrevious={handleClickPrevious}
-        onClickNext={handleClickNext}
-        onEnded={handleClickNext}
-        showSkipControls={true}
-        showJumpControls={false}
-        />
+
+      <div className={`${isPlaying ? "h-full " : null}`}>
+        <div className={`${isPlaying ? "h-screen absolute w-screen top-0 flex justify-center flex-col bg-primary-300 items-center ": "hidden" } `}>
+        <IonIcon name="close" className="text-white text-3xl top-0 left-0 absolute" onClick={() => playerHandler(!isPlaying)} />
+        <img src="https://cdn.britannica.com/84/206384-050-00698723/Javan-gliding-tree-frog.jpg" className="rounded-full h-32 w-32 " alt="" />
         </div>
+        <div>
+
+        <AudioPlayer
+          style={{
+            background: "linear-gradient(#ee0979, #341931)",
+            width: "100vw"    
+
+          }}
+          autoPlay
+          src={musicTracks[isTracking].src}
+          onPlay={musicTracks[isTracking].src}
+          header={`now playing ${musicTracks[isTracking].name}`}
+          footer={`from ${musicTracks[isTracking].creator}`}
+          onClickPrevious={handleClickPrevious}
+          onClickNext={handleClickNext}
+          onEnded={handleClickNext}
+          showSkipControls={true}
+          showJumpControls={false}
+          />
+          </div>
+      </div>
     </div>
   );
 };
