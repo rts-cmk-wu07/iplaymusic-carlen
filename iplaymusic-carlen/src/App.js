e860d3b286790753f3e967149b88d9cdb25d1a00
Carl Abel <caweb.cph@gmail.com>
2022-09-30 12:45:54 +0200

    toke not declared, hmm

--- iplaymusic-carlen/src/App.js
+++ iplaymusic-carlen/src/App.js
@@ -1,58 +1,80 @@
-import { vars } from "./vars"
-import { useState } from "react"
-import ColorContext from "./context/colorContext"
+import { vars } from "./vars";
+import { useState } from "react";
+import ColorContext from "./context/colorContext";
 
-import { BrowserRouter, Route, Routes } from "react-router-dom"
-import Layout from "./templates/Layout"
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Layout from "./templates/Layout";
 
-import Catagory from "./pages/Catagory"
-import HandleColorChange from "./context/handleColorChange"
-import AllAlbums from "./pages/AllAlbums"
-import AllArtists from "./pages/AllArtists"
-import EventFeed from "./pages/EventFeed"
-import Home from "./pages/Home"
-import Playlists from "./pages/Playlists"
+import Catagory from "./pages/Catagory";
+import HandleColorChange from "./context/handleColorChange";
+import AllAlbums from "./pages/AllAlbums";
+import AllArtists from "./pages/AllArtists";
+import EventFeed from "./pages/EventFeed";
+import Home from "./pages/Home";
+import Playlists from "./pages/Playlists";
 
-import Featured from "./templates/Featured"
-import TokenContext from "./context/TokenContext"
-import Login from "./templates/Login"
+import Featured from "./templates/Featured";
+import tokenContext from "./context/tokenContext";
+import Login from "./templates/Login";
 
 function App() {
-  const { light, dark } = vars
-  const [theme, setTheme] = useState(light)
-  const handleThemeChange = () => {
-    if (theme === light) {
-      setTheme(dark)
-    } else {
-      setTheme(light)
-    }
-  }
-  const colors = theme
-  const [token, setToken] = useState("")
+	const { light, dark } = vars;
+	const [theme, setTheme] = useState(light);
+	const handleThemeChange = () => {
+		if (theme === light) {
+			setTheme(dark);
+		} else {
+			setTheme(light);
+		}
+	};
+	const colors = theme;
+	const [token, setToken] = useState("");
 
-  return (
-    <TokenContext.Provider value={{ token, setToken }}>
-      <HandleColorChange.Provider value={handleThemeChange}>
-        <ColorContext.Provider value={colors}>
-          <BrowserRouter>
-            <Routes>
-              <Route path="/" element={!token ? <Login /> : <Layout />}>
-                <Route index element={<Home />} />
-                <Route path="/eventfeed" element={<EventFeed />} />
-                <Route path="/catagory" element={<Catagory />}>
-                  {" "}
-                </Route>
-                <Route path="/featured" element={<Featured />} />
-                <Route path="/allalbums" element={<AllAlbums />} />
-                <Route path="/allartists" element={<AllArtists />} />
-                <Route path="/playlists" element={<Playlists />} />
-              </Route>
-            </Routes>
-          </BrowserRouter>
-        </ColorContext.Provider>
-      </HandleColorChange.Provider>
-    </TokenContext.Provider>
-  )
+	return (
+		<tokenContext.Provider value={{ token, setToken }}>
+			<HandleColorChange.Provider value={handleThemeChange}>
+				<ColorContext.Provider value={colors}>
+					<BrowserRouter>
+						<Routes>
+							<Route
+								path="/"
+								element={!token ? <Login /> : <Layout />}>
+								<Route
+									index
+									element={<Home />}
+								/>
+								<Route
+									path="/eventfeed"
+									element={<EventFeed />}
+								/>
+								<Route
+									path="/catagory"
+									element={<Catagory />}>
+									{" "}
+								</Route>
+								<Route
+									path="/featured"
+									element={<Featured />}
+								/>
+								<Route
+									path="/allalbums"
+									element={<AllAlbums />}
+								/>
+								<Route
+									path="/allartists"
+									element={<AllArtists />}
+								/>
+								<Route
+									path="/playlists"
+									element={<Playlists />}
+								/>
+							</Route>
+						</Routes>
+					</BrowserRouter>
+				</ColorContext.Provider>
+			</HandleColorChange.Provider>
+		</tokenContext.Provider>
+	);
 }
 
-export default App
+export default App;
--- iplaymusic-carlen/src/context/tokenContext.js
+++ iplaymusic-carlen/src/context/tokenContext.js
@@ -1,3 +1,3 @@
-import { createContext } from "react"
-const tokenContext = createContext(null)
-export default tokenContext
+import { createContext } from "react";
+const tokenContext = createContext();
+export default tokenContext;
