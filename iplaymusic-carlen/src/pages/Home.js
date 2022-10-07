138ad0e6cf65e9c70d6107a8dfc7dfaada032ad9
CarlAbel <caweb.cph@gmail.com>
2022-10-07 14:10:08 +0200

    igennem til api

--- iplaymusic-carlen/src/pages/Home.js
+++ iplaymusic-carlen/src/pages/Home.js
@@ -1,6 +1,10 @@
 import Links from "../comp/Links";
+import { useContext } from "react";
+import TokenContext from "../context/TokenContext";
 
 const Home = () => {
+  var [token] = useContext(TokenContext);
+  console.log("home", token);
   return (
     <div>
       <Links color="blue" link="/eventfeed" linkText="Event feed" />
--- iplaymusic-carlen/src/templates/Login.js
+++ iplaymusic-carlen/src/templates/Login.js
@@ -1,55 +1,55 @@
-import querystring from "querystring";
-import Randomizer from "react-randomizer";
-import styled from "styled-components";
+import querystring from "querystring"
+import Randomizer from "react-randomizer"
+import styled from "styled-components"
 
 const Login = () => {
-	var state = Randomizer.randomNumber(1, 999999999);
-	var date = new Date();
-	date = date.setSeconds(date.getSeconds() + 20);
-	document.cookie = `state=${state}; expires=${date}`;
+  var state = Randomizer.randomNumber(1, 999999999)
+  var date = new Date()
+  date = date.setSeconds(date.getSeconds() + 99)
+  document.cookie = `state=${state}; expires=${date}`
 
-	var parameters = querystring.stringify({
-		response_type: "code",
-		client_id: "b85facc3edf64b4dbe90078275075248",
-		scope:
-			"user-read-private user-read-email user-library-read user-library-modify user-top-read user-follow-read user-read-recently-played user-read-currently-playing user-read-playback-state user-modify-playback-state user-read-playback-position ",
-		redirect_uri:
-			process.env.NODE_ENV === "production"
-				? process.env.REDIRECT_URI
-				: "http://localhost:8888/callback",
+  var parameters = querystring.stringify({
+    response_type: "code",
+    client_id: "b85facc3edf64b4dbe90078275075248",
+    scope:
+      "user-read-private user-read-email user-library-read user-library-modify user-top-read user-follow-read user-read-recently-played user-read-currently-playing user-read-playback-state user-modify-playback-state user-read-playback-position ",
+    redirect_uri:
+      process.env.NODE_ENV === "production"
+        ? process.env.REDIRECT_URI
+        : "http://localhost:8888/callback",
 
-		state,
-	});
+    state,
+  })
 
-	return (
-		<Container>
-			<a href={`https://accounts.spotify.com/authorize?${parameters}`}>
-				Login to spotify
-			</a>
-		</Container>
-	);
-};
-export default Login;
+  return (
+    <Container>
+      <a href={`https://accounts.spotify.com/authorize?${parameters}`}>
+        Login to spotify
+      </a>
+    </Container>
+  )
+}
+export default Login
 
 const Container = styled.div`
-	display: flex;
-	flex-direction: column;
-	align-items: center;
-	justify-content: center;
-	height: 100vh;
-	width: 100vw;
-	background-color: #341931;
-	gap: 5 rem;
-	img {
-		height: 20vh;
-	}
-	a {
-		padding: 1rem 5rem;
-		border-radius: 5rem;
-		border: none;
-		background-color: black;
-		color: #ff1168;
-		font-size: 1.4rem;
-		cursor: pointer;
-	}
-`;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  height: 100vh;
+  width: 100vw;
+  background-color: #341931;
+  gap: 5 rem;
+  img {
+    height: 20vh;
+  }
+  a {
+    padding: 1rem 5rem;
+    border-radius: 5rem;
+    border: none;
+    background-color: black;
+    color: #ff1168;
+    font-size: 1.4rem;
+    cursor: pointer;
+  }
+`
