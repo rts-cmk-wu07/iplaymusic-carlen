a7caa174ed5e971910d714d302d6e9537c16bc0d
CarlAbel <caweb.cph@gmail.com>
2022-10-10 10:44:22 +0200

    Lortet virker

--- src/comp/PlaylistsSongs.js
+++ src/comp/PlaylistsSongs.js
@@ -73,10 +73,8 @@ const PlaylistsSongs = (props) => {
 	var [tracks, setTracks] = useState([]);
 	useEffect(
 		function () {
-			
-			if (props.id)
 				axios
-					.get("https://api.spotify.com/v1/playlists/" + props.id + "/tracks", {
+					.get("https://api.spotify.com/v1/me/playlists", {
 						headers: {
 							Authorization: "Bearer " + token.access_token,
 						},
