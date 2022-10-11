f581c3742245c8c6112231809368f0f5eaa0ea27
CarlAbel <caweb.cph@gmail.com>
2022-10-11 08:36:19 +0200

    usefetch2

--- src/hooks/UseFetch.js
+++ src/hooks/UseFetch.js
@@ -1,7 +1,32 @@
-import React from 'react'
+import axios from "axios";
+import { useState, useEffect, useContext } from "react";
+import TokenContext from "../context/TokenContext";
 
-export default function UseFetch() {
-  return (
-    <div>UseFetch</div>
-  )
+export default function useFetch(url){
+    const [data, setData] = useState({});
+    const [error, setError] = useState(null)
+
+    const {tokenData, setTokenData} = useContext(TokenContext)
+    const { token } = tokenData
+
+    useEffect(() => {
+        if(!token) return;
+        if(!url) return;
+        axios.get(url, {
+            headers: {
+                Authorization: `Bearer ${token}`
+            },
+        
+})
+        .then((response) => {
+            if(response.status >= 200 && response.status <300) {
+                setData(response.data)
+            }
+         else {
+            setError(response.status);
+        }
+    });
+    }, [token, url, setTokenData]);
+
+    return {data, error};
 }
