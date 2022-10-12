8665d2082815d95bf5b917f90c49ae10648f7d4d
Cookieman2002 <mcl2002@outlook.dk>
2022-10-12 12:00:57 +0200

    it works first but fails a second

--- src/pages/AlbumDetails.js
+++ src/pages/AlbumDetails.js
@@ -3,7 +3,6 @@ import { css } from "@emotion/react";
 import { useContext, useState } from "react";
 import ColorContext from "../context/colorContext";
 import axios from "axios";
-import TagsRender from "../comp/sub-comp/TagsRender";
 import Heading from "../comp/sub-comp/Heading";
 import Song from "../comp/Song";
 import { useEffect } from "react";
@@ -12,6 +11,9 @@ import TokenContext from "../context/TokenContext";
 const AlbumDetails = () => {
   const amoSongs = 12;
   const colors = useContext(ColorContext);
+  const { id } = useParams();
+  const [singleAlbum, setSingleAlbum] = useState([]);
+  var [token] = useContext(TokenContext);
   const styles = {
     bgImage: css`
       padding: 12px;
@@ -22,9 +24,6 @@ const AlbumDetails = () => {
       color: ${colors.text};
     `,
   };
-  const { id } = useParams();
-  const [singleAlbum, setSingleAlbum] = useState([]);
-  var [token] = useContext(TokenContext);
   useEffect(
     function () {
       axios
@@ -41,20 +40,29 @@ const AlbumDetails = () => {
     },
     [token, setSingleAlbum]
   );
+  console.log(singleAlbum.tracks)
+  const  listOfSong = async () => {
+    return singleAlbum.tracks.items.map((items) => (
+      <Song
+        songtitle={items}
+        songtime="3:24"
+        artist="Billy Ray Cyrus"
+      />
+
+    ))
+  }
+  console.log(listOfSong());
   return (
+    
     <div>
       <div css={styles.bgImage}>
         <Heading text={singleAlbum.name} color="white" />
-        <p className="text-white">{amoSongs} Songs</p>
+        <p className="text-white">{singleAlbum.total_tracks} Songs</p>
       </div>
       <section className="p-3">
         <h3 css={styles.theme}>All songs</h3>
         <div className="flex flex-col w-full">
-          <Song
-            songtitle="Country road"
-            songtime="3:24"
-            artist="Billy Ray Cyrus"
-          />
+          {listOfSong()}
         </div>
       </section>
     </div>
